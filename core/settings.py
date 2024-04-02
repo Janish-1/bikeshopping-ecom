7c3383e6681263c73041513a842a1cf82a527d6d
Janish-1 <janish.pancholi11@gmail.com>
2024-04-02 19:00:46 +0530

    possible fix

--- core/settings.py
+++ core/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'your_app/static'),
+    os.path.join(BASE_DIR, 'home/static'),
 ]
+
+STATIC_ROOT = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
